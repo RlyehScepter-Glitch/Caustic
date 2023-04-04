5b5eb76cf2234eb44e75f588dd73792e7ecf2f5e
RlyehScepter <rlyehscepter@gmail.com>
2023-04-04 16:51:26 +0300

    Main generates .PPM image of a pyramid for homework

--- Caustic/Main.cpp
+++ Caustic/Main.cpp
@@ -1,5 +1,6 @@
 #include "Vec3.h"
 #include "Ray.h"
+#include "Triangle.h"
 
 #include <fstream>
 #include <math.h>
@@ -11,12 +12,22 @@ static const int maxColorComponent = 255;
 
 int main() 
 {
-	std::ofstream ppmFileStream("CRT_Homework_3.ppm", std::ios::out | std::ios::binary);
+	std::ofstream ppmFileStream("HomeworkNo5.ppm", std::ios::out | std::ios::binary);
 	ppmFileStream << "P3\n";
 	ppmFileStream << imageWidth << " " << imageHeight << "\n";
 	ppmFileStream << maxColorComponent << "\n";
 
-	Caustic::Vec3 origin(0, 0, 0);
+	Caustic::Vec3 origin(0.0f, 0.5f, 0.0f);
+	Caustic::Vec3 P1(-1.0f, 0.0f, -2.0f);
+	Caustic::Vec3 P2(0.0f, 0.0f, -1.0f);
+	Caustic::Vec3 P3(1.0f, 0.0f, -2.0f);
+	Caustic::Vec3 P4(0.0f, 0.0f, -3.0f);
+	Caustic::Vec3 P5(0.0f, 1.0f, -2.0f);
+
+	Caustic::Triangle T1(P1, P2, P5);
+	Caustic::Triangle T2(P2, P3, P5);
+	Caustic::Triangle T3(P3, P4, P5);
+	Caustic::Triangle T4(P4, P1, P5);
 
 	for (int y = 0; y < imageHeight; y++)
 	{
@@ -24,26 +35,48 @@ int main()
 		{
 			float tempX = x;
 			float tempY = y;
-
+			
 			//Find center based on raster coordinates
 			tempX += 0.5f;
 			tempY += 0.5f;
-
+			
 			//Convert raster coordinates to NDC space [0.0, 1.0]
 			tempX /= imageWidth;
 			tempY /= imageHeight;
-
+			
 			//Convert NDC coordinates to Screen space [-1.0, 1.0]
 			tempX = (2.0f * tempX) - 1.0f;
 			tempY = 1.0f - (2.0f * tempY);
-
+			
 			//Aspect ratio
 			tempX *= imageWidth / imageHeight;
-
+			
 			Caustic::Vec3 rayDir(tempX, tempY, -1);
 			rayDir.Normalize();
+			
+			Caustic::Ray R(origin, rayDir);
+			Caustic::HitRecord hitRecord;
 
-			ppmFileStream << abs(rayDir.getX() * 255) << " " << abs(rayDir.getY() * 255) << " " << abs(rayDir.getZ() * 255) << "\t";
+			if(T1.Intersect(R, hitRecord))
+			{
+				ppmFileStream << 255 << " " << 0 << " " << abs(hitRecord.m_IntersectionPoint.getZ() * 255) << "\t";
+			}
+			else if(T2.Intersect(R, hitRecord))
+			{
+				ppmFileStream << 0 << " " << 255 << " " << abs(hitRecord.m_IntersectionPoint.getZ() * 255) << "\t";
+			}
+			else if(T3.Intersect(R, hitRecord))
+			{
+				ppmFileStream << 0 << " " << abs(hitRecord.m_IntersectionPoint.getY() * 255) << " " << 255 << "\t";
+			}
+			else if(T4.Intersect(R, hitRecord))
+			{
+				ppmFileStream << 255 << " " << abs(hitRecord.m_IntersectionPoint.getY() * 255) << " " << 0 << "\t";
+			}
+			else
+			{
+				ppmFileStream << abs(rayDir.getX() * 255) << " " << abs(rayDir.getY() * 255) << " " << abs(rayDir.getZ() * 255) << "\t";
+			}
 		}
 
 		ppmFileStream << "\n";
