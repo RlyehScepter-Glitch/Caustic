f6f90176ffc5b1541702083f76beeffb4303e258
RlyehScepter <rlyehscepter@gmail.com>
2023-04-25 17:58:17 +0300

    Setters for member variables added to Settings class

--- Caustic/src/Scene/Settings.cpp
+++ Caustic/src/Scene/Settings.cpp
@@ -4,5 +4,22 @@ namespace Caustic
 {
 	Settings::Settings(const glm::vec3& backgroundColor, uint32_t width, uint32_t height)
 		:m_BackgroundColor(backgroundColor), m_Width(width), m_Height(height)
-	{}
+	{
+	}
+
+	void Settings::SetBackgroundColor(const glm::vec3& bgColor)
+	{
+		m_BackgroundColor = bgColor;
+	}
+
+	void Settings::SetWidth(uint32_t width)
+	{
+		m_Width = width;
+	}
+
+	void Settings::SetHeight(uint32_t height)
+	{
+		m_Height = height;
+	}
+
 }
--- Caustic/src/Scene/Settings.h
+++ Caustic/src/Scene/Settings.h
@@ -7,9 +7,19 @@ namespace Caustic
 	class Settings
 	{
 	public:
+		//Constructors
+		//Default
+		Settings() = default;
+		//Takes Background Color, Width and Height
 		Settings(const glm::vec3& backgroundColor, uint32_t width, uint32_t height);
+
+		//Setters
+		void SetBackgroundColor(const glm::vec3& bgColor);
+		void SetWidth(uint32_t width);
+		void SetHeight(uint32_t height);
+
 	private:
-		glm::vec3 m_BackgroundColor;
-		uint32_t m_Width, m_Height;
+		glm::vec3 m_BackgroundColor = { 0.0f, 0.0f, 0.0f };
+		uint32_t m_Width = 1920, m_Height = 1080;
 	};
 }
