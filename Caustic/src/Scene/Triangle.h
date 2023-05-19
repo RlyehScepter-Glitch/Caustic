4e854a903ba63be85b03b2bb302d15ade2fd2be6
RlyehScepter <rlyehscepter@gmail.com>
2023-05-19 16:13:39 +0300

    Triangle member variable Albedo renamed back to color

--- Caustic/src/Scene/Triangle.h
+++ Caustic/src/Scene/Triangle.h
@@ -15,13 +15,13 @@ namespace Caustic
         bool ShadowIntersect(const Ray& ray, float& t, float& u, float& v) const;
 
         const glm::vec3 GetNormal() const { return m_Normal; }
-        const glm::vec3 GetAlbedo() const { return m_Albedo; }
+        const glm::vec3 GetColor() const { return m_Color; }
     private:
         glm::vec3 m_V0;  // First vertex
         glm::vec3 m_V1;  // Second vertex
         glm::vec3 m_V2;  // Third vertex
         glm::vec3 m_Normal;  // Surface normal
-        glm::vec3 m_Albedo; // Albedo
+        glm::vec3 m_Color; // Albedo
         float m_Area;
     };
 
