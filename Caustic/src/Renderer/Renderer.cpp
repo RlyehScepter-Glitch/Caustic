7a713e05867e141097d6f56d41a33505e2129e4f
RlyehScepter <rlyehscepter@gmail.com>
2023-05-29 19:06:02 +0300

    -Added Shading functionality to Renderer Class

--- Caustic/src/Renderer/Renderer.cpp
+++ Caustic/src/Renderer/Renderer.cpp
@@ -3,10 +3,12 @@
 #include <fstream>
 #include <iostream>
 #include <algorithm>
+#include <glm/glm.hpp>
 #include <glm/gtc/constants.hpp>
 
-static const int maxColorComponent = 255;
-static const float shadowBias = 0.001f;
+static const uint32_t maxColorComponent = 255;
+static const float shadowBias = 0.1f;
+static const uint32_t maxRayDepth = 5;
 
 namespace Caustic
 {
@@ -16,15 +18,9 @@ namespace Caustic
 		Caustic::Settings sceneSettings = scene.GetSettings();
 		uint32_t sceneWidth = sceneSettings.GetWidth();
 		uint32_t sceneHeight = sceneSettings.GetHeight();
-		float aspectRatio = sceneSettings.GetAspectRatio();
 		glm::mat3 viewMatrix = (glm::mat3)scene.GetCamera().GetViewMatrix();
-		glm::vec3 background = scene.GetSettings().GetBackgroundColor();
-		float bgR = background.r;
-		float bgG = background.g;
-		float bgB = background.b;
 
 		// Open Filestream
-
 		std::ofstream ppmFileStream(fileName, std::ios::out | std::ios::binary);
 		ppmFileStream << "P3\n";
 		ppmFileStream << sceneWidth << " " << sceneHeight << "\n";
@@ -33,83 +29,20 @@ namespace Caustic
 		// Create a two-dimensional array to represent the image
 		std::vector<std::vector<glm::vec3>> image(sceneHeight, std::vector<glm::vec3>(sceneWidth));
 
-		// Render image
+		// Fill in image data
 		for (uint32_t y = 0; y < sceneHeight; y++)
 		{
 			for (uint32_t x = 0; x < sceneWidth; x++)
 			{
 				// Ray Generation
-				Ray R = Renderer::GenerateRay(x, y, scene);
+				Ray R = Renderer::GenerateCameraRay(x, y, scene);
 
-				float t = 0.0f;
-				float tNear = FLT_MAX;
-				uint32_t triangleIndex;
-				glm::vec2 uv(0, 0);
+				// Trace Ray
+				IntersectionData data = scene.TraceRay(R);
 
-				glm::vec3 backgroundColor(bgR, bgG, bgB);
-
-				//-----Ray Triangle Intersection-----//
-				// Check every Mesh for intersection with Camera Ray
-				for (const Caustic::Mesh& mesh : scene.GetObjects())
-				{
-					if (mesh.Intersect(R, scene.GetCamera().GetNearClip(), tNear, triangleIndex, uv))
-					{
-						Caustic::Triangle triangle = mesh.GetTriangles()[triangleIndex];
-						glm::vec3 hitPoint = R.GetOrigin() + R.GetDirection() * tNear;
-
-						glm::vec3 finalColor(0.0f, 0.0f, 0.0f);
-						glm::vec3 triangleColor(uv.x, uv.y, 1 - uv.x - uv.y);
-
-						// Check if Scene containse lights
-						if (scene.GetLights().size() == 0)
-						{
-							finalColor = triangleColor;
-						}
-						else
-						{
-							for (const Caustic::Light& light : scene.GetLights())
-							{
-								glm::vec3 lightDir = light.GetPosition() - hitPoint;
-								float sphereRad = glm::length(lightDir);
-								lightDir = glm::normalize(lightDir);
-								float cosLaw = glm::max(0.0f, glm::dot(lightDir, triangle.GetNormal()));
-								float sphereArea = 4.0f * glm::pi<float>() * sphereRad * sphereRad;
-
-								float lightIntensity = light.GetIntensity();
-
-								Caustic::Ray shadowRay(hitPoint + triangle.GetNormal() * shadowBias, lightDir);
-
-								glm::vec3 lightContribution(0.0f);
-
-								bool shadowRayIntersect = false;
-
-								// Check if Shadow ray intersects anything
-								for (const Caustic::Mesh& meshAgain : scene.GetObjects())
-								{
-									if (meshAgain.ShadowIntersect(shadowRay))
-									{
-										shadowRayIntersect = true;
-										break;
-									}
-								}
-
-								if (shadowRayIntersect == false)
-								{
-									lightContribution = lightIntensity / sphereArea * triangleColor * cosLaw;
-								}
-
-								finalColor += lightContribution;
-							}						
-						}
-
-						backgroundColor = finalColor;
-					}
-				}
-
-				image[y][x] = backgroundColor;
+				// Shade Pixel
+				image[y][x] = Renderer::Shade(R, data, scene);
 			}
-
-
 		}
 
 		// Write the image to file
@@ -127,7 +60,7 @@ namespace Caustic
 		ppmFileStream.close();
 	}
 	
-	Ray Renderer::GenerateRay(const uint32_t& x, const uint32_t& y, const Scene& scene)
+	Ray Renderer::GenerateCameraRay(const uint32_t& x, const uint32_t& y, const Scene& scene)
 	{
 		//-----Ray Generation----//
 		float tempX = x;
@@ -151,8 +84,146 @@ namespace Caustic
 		glm::vec3 rayDir(tempX, tempY, -1);
 		rayDir = (glm::mat3)scene.GetCamera().GetViewMatrix() * glm::normalize(rayDir);
 
-		Caustic::Ray ray(scene.GetCamera().GetPosition(), rayDir);
+		Caustic::Ray ray(scene.GetCamera().GetPosition(), rayDir, RayType::camera);
 
 		return ray;
+	}
+	
+	glm::vec3 Renderer::Shade(const Ray& ray, const IntersectionData& data, const Scene& scene)
+	{
+		glm::vec3 finalColor(0.0f, 0.0f, 0.0f);
+		if (ray.GetDepth() >= maxRayDepth || data.triangleIdx == -1)
+		{
+			float depthMulti = 1.0f - 0.2f * ray.GetDepth();
+			finalColor = scene.GetSettings().GetBackgroundColor() * depthMulti;
+			return finalColor;
+		}
+
+		Material mat = scene.GetMaterials()[data.materialIdx];
+
+		if(mat.GetMaterialType() == MaterialType::diffuse)
+		{
+			finalColor = ShadeDiffuse(ray, data, scene);
+		}
+		else if (mat.GetMaterialType() == MaterialType::reflective)
+		{
+			finalColor = ShadeReflective(ray, data, scene);
+		}
+		else if (mat.GetMaterialType() == MaterialType::refractive)
+		{
+			//TBI
+			finalColor = ShadeRefractive(ray, data, scene);
+		}
+		else if (mat.GetMaterialType() == MaterialType::constant)
+		{
+			//TBI
+			finalColor = ShadeConstant(ray, data, scene);
+		}
+		else
+		{
+			assert(false);
+		}
+
+		return finalColor;
+	}
+	
+	glm::vec3 Renderer::ShadeDiffuse(const Ray& ray, const IntersectionData& data, const Scene& scene)
+	{
+		float u = data.UV.x;
+		float v = data.UV.y;
+		float w = 1 - data.UV.x - data.UV.y;
+
+		// Color Based on UV
+		//glm::vec3 triangleColor(u, v, w);
+		// Flat Color
+		//glm::vec3 triangleColor(0.8f, 0.0f, 0.3f);
+		// Internal Triangle Color
+		glm::vec3 triangleColor = scene.GetObjects()[data.objectIdx].GetTriangles()[data.triangleIdx].GetColor();
+
+		glm::vec3 diffuseColor(0.0f, 0.0f, 0.0f);
+
+		for (const Caustic::Light& light : scene.GetLights())
+		{
+			glm::vec3 lightDir;
+			glm::vec3 originOffset;
+			float cosLaw;
+			float sphereRad;
+
+			if (scene.GetMaterials()[data.materialIdx].SmoothShading())
+			{
+				lightDir = light.GetPosition() - data.hitPoint;
+				sphereRad = glm::length(lightDir);
+				lightDir = glm::normalize(lightDir);
+				cosLaw = glm::max(0.0f, glm::dot(lightDir, data.interpolatedVertexNormal));
+				originOffset = data.hitPoint + data.interpolatedVertexNormal * shadowBias;
+			}
+			else
+			{
+				lightDir = light.GetPosition() - data.hitPoint;
+				sphereRad = glm::length(lightDir);
+				lightDir = glm::normalize(lightDir);
+				cosLaw = glm::max(0.0f, glm::dot(lightDir, data.hitPointNormal));
+				originOffset = data.hitPoint + data.hitPointNormal * shadowBias;
+			}
+			
+			float lightIntensity = light.GetIntensity();
+
+			Caustic::Ray shadowRay(originOffset, lightDir, RayType::shadow);
+
+			float sphereArea = 4.0f * glm::pi<float>() * sphereRad * sphereRad;
+
+			glm::vec3 lightContribution(0.0f, 0.0f, 0.0f);
+
+			// Check if Shadow ray intersects anything
+			IntersectionData shadowData = scene.TraceRay(shadowRay);
+			
+			if(shadowData.triangleIdx == -1)
+			{
+				lightContribution = lightIntensity / sphereArea * triangleColor * cosLaw;
+			}
+
+			diffuseColor += lightContribution;
+		}
+
+		return diffuseColor;
+	}
+
+	glm::vec3 Renderer::ShadeReflective(const Ray& ray, const IntersectionData& data, const Scene& scene)
+	{
+		bool smoothShading = scene.GetMaterials()[data.materialIdx].SmoothShading();
+
+		glm::vec3 originOffset;
+		glm::vec3 N;
+		glm::vec3 A = ray.GetDirection();
+
+		if(smoothShading)
+		{
+			originOffset = data.interpolatedVertexNormal * shadowBias;
+			N = data.interpolatedVertexNormal;
+		}
+		else
+		{
+			originOffset = data.hitPointNormal * shadowBias;
+			N = data.hitPointNormal;
+		}
+		
+		glm::vec3 reflOrigin = data.hitPoint + originOffset;
+		glm::vec3 reflDir = A - 2 * glm::dot(A, N) * N;
+		
+		Ray reflRay(reflOrigin, reflDir, RayType::reflection, ray.GetDepth() + 1);
+		IntersectionData reflData = scene.TraceRay(reflRay);
+		glm::vec3 reflectColor = Shade(reflRay, reflData, scene);
+
+		return reflectColor;
+	}
+	
+	glm::vec3 Renderer::ShadeRefractive(const Ray& ray, const IntersectionData& data, const Scene& scene)
+	{
+		return glm::vec3();
+	}
+	
+	glm::vec3 Renderer::ShadeConstant(const Ray& ray, const IntersectionData& data, const Scene& scene)
+	{
+		return glm::vec3();
 	}
 }
--- Caustic/src/Renderer/Renderer.h
+++ Caustic/src/Renderer/Renderer.h
@@ -4,12 +4,16 @@
 
 namespace Caustic
 {
-
 	static class Renderer
 	{
 	public:
 		static void GenerateImage(const Scene& scene, const std::string& fileName);
-		static Ray GenerateRay(const uint32_t& x, const uint32_t& y, const Scene& scene);
+		static Ray GenerateCameraRay(const uint32_t& x, const uint32_t& y, const Scene& scene);
+		static glm::vec3 Shade(const Ray& ray, const IntersectionData& data, const Scene& scene);
+		static glm::vec3 ShadeDiffuse(const Ray& ray, const IntersectionData& data, const Scene& scene);
+		static glm::vec3 ShadeReflective(const Ray& ray, const IntersectionData& data, const Scene& scene);
+		static glm::vec3 ShadeRefractive(const Ray& ray, const IntersectionData& data, const Scene& scene);
+		static glm::vec3 ShadeConstant(const Ray& ray, const IntersectionData& data, const Scene& scene);
 	};
 
 }
